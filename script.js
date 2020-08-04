6321c62f6d2c2f83dd9a96664c15b8fee841ba5e
Suvendu97 <57838911+Suvendu97@users.noreply.github.com>
2020-08-04 21:23:43 +0530

    adding comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+
+// getting the first elements
 const submitForm = document.querySelector('.add');
 const addButton = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todos');
 const list = document.querySelectorAll('.todos li'); 
 
+// lenth of my Todo list
 let listLenght = list.lenght;
  // document.getElementById("total").innerHTML= listLenght;
 
+// adding new elements in my list item
 const generateTempalate = (todo) => {
   
   const html = `<li>
@@ -19,6 +23,8 @@ const generateTempalate = (todo) => {
   todoList.innerHTML += html;
 };
 
+
+// adding tast func when getting the new input value
 function addTodos(e) {
   e.preventDefault();
   const todo = submitForm.add.value.trim();
@@ -31,3 +37,5 @@ function addTodos(e) {
 
 submitForm.addEventListener('submit', addTodos);
 addButton.addEventListener('click', addTodos);
+
+
